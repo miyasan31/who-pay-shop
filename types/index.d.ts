979d79adb1f07c23cca83d7fc066ff83eb02d708
miyasan_0301 <miyasan.19990301@gmail.com>
2021-11-29 10:34:12 +0900

    🏷️

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,13 +8,19 @@
 
 import type { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import type { CompositeScreenProps, NavigatorScreenParams } from "@react-navigation/native";
-import type { NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 declare global {
 	namespace ReactNavigation {
 		type RootParamList = RootStackParamList;
 	}
 }
+
+export type RootTabParamList = {
+	TabOne: undefined;
+	TabTwo: undefined;
+	TabThree: undefined;
+};
 
 export type RootStackParamList = {
 	Root: NavigatorScreenParams<RootTabParamList> | undefined;
@@ -23,25 +29,17 @@ export type RootStackParamList = {
 	Signup: undefined;
 	Verify: { phone: string };
 
+	Record: { price: string };
+	Calculator: undefined;
+
 	Modal1: undefined;
 	Modal2: undefined;
 	NotFound: undefined;
 };
 
-export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
-	RootStackParamList,
-	Screen
->;
+export type StackScreenProps<T extends keyof RootStackParamList> = NativeStackScreenProps<RootStackParamList, T>;
 
-export type RootTabParamList = {
-	TabOne: undefined;
-	TabTwo: undefined;
-	TabThree: undefined;
-};
-
-export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
-	BottomTabScreenProps<RootTabParamList, Screen>,
+export type TabScreenProps<T extends keyof RootTabParamList> = CompositeScreenProps<
+	BottomTabScreenProps<RootTabParamList, T>,
 	NativeStackScreenProps<RootStackParamList>
 >;
-
-type ScreenProp = NativeStackNavigationProp<RootStackParamList>;
