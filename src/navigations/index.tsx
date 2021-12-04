457df18315210438e816a48eb25c7dec4fb88753
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-04 11:24:44 +0900

    ♻️ themeをmemo化

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -4,17 +4,18 @@ import {
 	NavigationContainer,
 } from "@react-navigation/native";
 import type { VFC } from "react";
-import React from "react";
+import React, { useMemo } from "react";
 import type { ColorSchemeName } from "react-native";
 import { LinkingConfiguration } from "src/navigations/LinkingConfiguration";
 import { RootNavigator } from "src/navigations/RootNavigator";
 
 export const Navigation: VFC<{ colorScheme: ColorSchemeName }> = (props) => {
+	const themeResult = useMemo(() => {
+		return props?.colorScheme === "dark" ? DarkTheme : DefaultTheme;
+	}, [props]);
+
 	return (
-		<NavigationContainer
-			linking={LinkingConfiguration}
-			theme={props?.colorScheme === "dark" ? DarkTheme : DefaultTheme}
-		>
+		<NavigationContainer linking={LinkingConfiguration} theme={themeResult}>
 			<RootNavigator />
 		</NavigationContainer>
 	);
