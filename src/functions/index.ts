af6cfbc9685407631690a9ed013b5e56f553bb78
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-04 20:46:25 +0900

    ✨ sequreStore追加

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,4 +1,13 @@
+import { deleteSequreStore } from "./deleteSequreStore";
 import { fetcher } from "./fetcher";
+import { getSequreStore } from "./getSequreStore";
 import { onKeyBoardClose } from "./onKeyBoardClose";
+import { saveSequreStore } from "./saveSequreStore";
 
-export { fetcher, onKeyBoardClose };
+export {
+	deleteSequreStore,
+	fetcher,
+	getSequreStore,
+	onKeyBoardClose,
+	saveSequreStore,
+};
