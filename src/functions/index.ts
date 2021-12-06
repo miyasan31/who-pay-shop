b7dd11c018ebae800b2b22b987b1a5b9b8b8bc81
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-06 11:46:33 +0900

    🚚  フォルダ変更

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,13 +1,3 @@
-import { deleteSequreStore } from "./deleteSequreStore";
-import { fetcher } from "./fetcher";
-import { getSequreStore } from "./getSequreStore";
 import { onKeyBoardClose } from "./onKeyBoardClose";
-import { saveSequreStore } from "./saveSequreStore";
 
-export {
-	deleteSequreStore,
-	fetcher,
-	getSequreStore,
-	onKeyBoardClose,
-	saveSequreStore,
-};
+export { onKeyBoardClose };
