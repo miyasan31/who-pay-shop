6b835c31731b45c97321638214ad4a3f7a68d213
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-05 16:44:44 +0900

    ✨ hook/useMonthPagenation

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ import { useCachedResources } from "./useCachedResources";
 import { useColorScheme } from "./useColorScheme";
 import { useGetSWR } from "./useGetSWR";
 import { useGetSWRdev } from "./useGetSWRdev";
+import { useMonthPagenation } from "./useMonthPagenation";
 import { useSignout } from "./useSignout";
 import { useTab } from "./useTab";
 import { useThemeColor } from "./useThemeColor";
@@ -11,6 +12,7 @@ export {
 	useColorScheme,
 	useGetSWR,
 	useGetSWRdev,
+	useMonthPagenation,
 	useSignout,
 	useTab,
 	useThemeColor,
