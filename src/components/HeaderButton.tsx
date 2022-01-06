3349fc40c3796f7435e476c6fc51b1de6eb39d3a
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-06 12:12:44 +0900

    🚧 HeaderButton

---
+++ src/components/HeaderButton.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { MaterialIcons } from "@expo/vector-icons";
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+import type { VFC } from "react";
+import React, { useCallback } from "react";
+import { Pressable, StyleSheet } from "react-native";
+import { useThemeColor } from "src/hooks";
+import type {
+	MainScreenProps,
+	PayScreenProps,
+	SettingScreenProps,
+} from "types";
+
+type Props = (
+	| MainScreenProps<"Setting">
+	| PayScreenProps<"Calculator">
+	| SettingScreenProps<"SettingSelect">
+) & {
+	screen: "Setting" | "Pay";
+};
+
+export const HeaderButton: VFC<Props> = (props) => {
+	const icon1 = useThemeColor({}, "icon1");
+
+	const onPrevScreen = useCallback((navigation) => {
+		navigation.replace(props.screen);
+	}, []);
+
+	return (
+		<Pressable
+			onPress={() => onPrevScreen(props.navigation)}
+			style={({ pressed }) => [{ opacity: pressed ? 0.4 : 1 }, styles.prev]}
+		>
+			<MaterialIcons name="settings" size={24} color={icon1} />
+		</Pressable>
+	);
+};
+
+const styles = StyleSheet.create({
+	buttonLabel: {
+		fontWeight: "400",
+	},
+	prev: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "flex-end",
+
+		width: 40,
+		marginLeft: 20,
+	},
+});
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { CircleKeyButton } from "./CircleKeyButton";
 import { ErrorMessage } from "./ErrorMessage";
+import { HeaderButton } from "./HeaderButton";
 import { KeyButton } from "./KeyButton";
 import { ListItem } from "./ListItem";
 import { Progress } from "./Progress";
@@ -8,6 +9,7 @@ import { Toaster } from "./Toaster";
 export {
 	CircleKeyButton,
 	ErrorMessage,
+	HeaderButton,
 	KeyButton,
 	ListItem,
 	Progress,
