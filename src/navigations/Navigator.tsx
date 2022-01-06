a450ce9447871afa02a660ca07cc35c6b045ef61
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-06 12:14:10 +0900

    ♻️

---
+++ src/navigations/Navigator.tsx
@@ -0,0 +1,25 @@
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { useThemeColor } from "src/hooks";
+import { MainNavigator } from "src/screens/main";
+import type { RootStackParamList } from "types";
+
+// const Drawer = createDrawerNavigator<RootStackParamList>();
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+export const Navigator = () => {
+	const backgroundColor = useThemeColor({}, "bg1");
+	return (
+		<RootStack.Navigator initialRouteName="Main">
+			<RootStack.Screen
+				name="Main"
+				component={MainNavigator}
+				options={{
+					headerShown: false,
+					headerStyle: { backgroundColor: backgroundColor },
+				}}
+			/>
+		</RootStack.Navigator>
+	);
+};
