600770129a6bed63a2cf42e0ebb1e57550ad1d32
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-08 01:44:58 +0900

    ♻️

--- src/hooks/useGetSWR.ts
+++ src/hooks/useGetSWR.ts
@@ -1,22 +1,21 @@
-import { getFetcher } from "src/functions/fetcher";
 import useSWRImmutable from "swr/immutable";
 
-export const useGetSWR = <T>(url: string) => {
-  const { data, error } = useSWRImmutable<T>(url, getFetcher);
+export const useGetSWR = <T>(url: string, options?: any) => {
+	const { data, error } = useSWRImmutable<T>(url, options);
 
-  return {
-    data: data,
-    isError: error,
-    isLoading: !error && !data,
-  };
+	return {
+		data: data,
+		isError: error,
+		isLoading: !error && !data,
+	};
 };
 
 export const useGetSWRWithToken = <T>(url: string, token: string) => {
-  const { data, error } = useSWRImmutable<T>([url, token]);
+	const { data, error } = useSWRImmutable<T>([url, token]);
 
-  return {
-    data: data,
-    isError: error,
-    isLoading: !error && !data,
-  };
+	return {
+		data: data,
+		isError: error,
+		isLoading: !error && !data,
+	};
 };
